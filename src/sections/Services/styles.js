13dca259ca2fbbe3d9ccfa699d842d9c5c106e7b
wfercanas <fernando.canas@outlook.com>
2022-05-17 14:56:32 -0500

    add/ services section

---
+++ src/sections/Services/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledServiceCardsContainer = styled.div`
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+`;
+
+export { StyledServiceCardsContainer };
