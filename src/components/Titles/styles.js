e67b589deca7868e834aad2ceb8140dc89d15663
wfercanas <fernando.canas@outlook.com>
2022-05-17 11:27:50 -0500

    add/ section title and subtitles with styles

---
+++ src/components/Titles/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledSectionTitle = styled.h2`
+  font-size: 16px;
+  font-weight: normal;
+  color: darkslategray;
+`;
+
+const StyledSectionSubtitle = styled.p`
+  font-size: 32px;
+  font-weight: bold;
+  max-width: 60%;
+  line-height: 44px;
+  text-transform: capitalize;
+`;
+
+export { StyledSectionTitle, StyledSectionSubtitle };
