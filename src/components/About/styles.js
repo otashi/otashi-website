cdd903f1153096fc8ad248fe8c2c5d2618f49c15
wfercanas <fernando.canas@outlook.com>
2022-05-16 23:56:16 -0500

    add/ component list item in about section

---
+++ src/components/About/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledAboutContentListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`;
+
+const StyledCheck = styled.img`
+  width: 18px;
+`;
+
+export { StyledAboutContentListItem, StyledCheck };
