e00592e85b4ecc9b1d32b23820a7f8c2207d972b
wfercanas <fernando.canas@outlook.com>
2022-05-17 17:10:20 -0500

    add/ id prop for sections

--- src/containers/Sections/index.jsx
+++ src/containers/Sections/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { StyledSectionContainer } from "./styles";
 
-const SectionContainer = ({ children }) => {
-  return <StyledSectionContainer>{children}</StyledSectionContainer>;
+const SectionContainer = ({ children, id }) => {
+  return <StyledSectionContainer id={id}>{children}</StyledSectionContainer>;
 };
 
 export { SectionContainer };
