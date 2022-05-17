0f8e1742153d4d87f9abaadc3a0767ce37033460
wfercanas <fernando.canas@outlook.com>
2022-05-17 11:27:34 -0500

    add/ sections container render children

---
+++ src/containers/Sections/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StyledSectionContainer } from "./styles";
+
+const SectionContainer = ({ children }) => {
+  return <StyledSectionContainer>{children}</StyledSectionContainer>;
+};
+
+export { SectionContainer };
---
+++ src/containers/Sections/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledSectionContainer = styled.section`
+  margin: 100px 0;
+`;
+
+export { StyledSectionContainer };
