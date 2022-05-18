dcf1f01f0a3ff9dadd126ccee3a3404123d39934
wfercanas <fernando.canas@outlook.com>
2022-05-18 14:58:14 -0500

    add/ Logo component with pixels as props

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StyledLogo } from "./styles";
+
+const Logo = ({ px }) => {
+  return <StyledLogo px={px}>Otashi</StyledLogo>;
+};
+
+export { Logo };
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledLogo = styled.i`
+  color: var(--t100);
+  font-size: ${({ px }) => `${px}px`};
+  font-style: normal;
+`;
+
+export { StyledLogo };
