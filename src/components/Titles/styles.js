b1335da89c315909df78dc82984f67451dc1de05
wfercanas <fernando.canas@outlook.com>
2022-05-18 15:39:28 -0500

    refactor/ fontWeight and color of section titles

--- src/components/Titles/styles.js
+++ src/components/Titles/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const StyledSectionTitle = styled.h2`
   font-size: 16px;
-  font-weight: normal;
-  color: darkslategray;
+  font-weight: bold;
+  color: var(--t500);
 `;
 
 const StyledSectionSubtitle = styled.p`
