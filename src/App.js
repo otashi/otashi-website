9ae576181e5c152e2f62c1ec89b0a43016798557
wfercanas <fernando.canas@outlook.com>
2022-05-17 20:23:53 -0500

    add/ app with intro, about and services sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import "./globals.css";
+
+import { Header } from "./components/Header/";
+import { IntroContainer } from "./sections/Intro";
+import { AboutSection } from "./sections/About";
+import { ServicesSection } from "./sections/Services";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <IntroContainer />
+        <AboutSection id="about-us" />
+        <ServicesSection />
+        <section id="value-proposition"></section>
+        <section id="contact"></section>
+      </main>
+      <footer>
+        <img src="" alt="Otashi" />
+      </footer>
+    </>
   );
 }
 
