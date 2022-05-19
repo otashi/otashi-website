7aa07ec2720a7725682e9eef03ebda61df240954
wfercanas <fernando.canas@outlook.com>
2022-05-19 18:18:55 -0500

    add/ media query padding-top of 64px for sections

--- src/containers/Sections/styles.js
+++ src/containers/Sections/styles.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const StyledSectionContainer = styled.section`
   padding: 100px 0 0;
   color: var(--n900);
+
+  @media screen and (max-width: 550px) {
+    padding-top: 64px;
+  }
 `;
 
 export { StyledSectionContainer };
