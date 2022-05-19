472019941389750fe5322e76b010c563f113a824
wfercanas <fernando.canas@outlook.com>
2022-05-19 18:38:40 -0500

    add/ responsive about section

--- src/sections/About/styles.js
+++ src/sections/About/styles.js
@@ -4,11 +4,21 @@ const StyledAboutGrid = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
   column-gap: 32px;
+
+  @media screen and (max-width: 1100px) {
+    grid-template-columns: 350px 1fr;
+  }
+
+  @media screen and (max-width: 850px) {
+    grid-template-columns: auto;
+    grid-template-rows: auto auto;
+    row-gap: 48px;
+  }
 `;
 
 const StyledFigure = styled.figure`
   max-width: 500px;
-  margin: 0;
+  margin: 0 auto;
 `;
 
 const StyledImage = styled.img`
@@ -21,6 +31,10 @@ const StyledAboutContent = styled.div``;
 const StyledAboutContentText = styled.p`
   margin: 0 0 80px;
   line-height: 26px;
+
+  @media screen and (max-width: 1100px) {
+    margin-bottom: 42px;
+  }
 `;
 
 const StyledSpan = styled.span`
