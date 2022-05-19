f589f11d79b4c7766e9dc245d3a2b5bc8be16e68
wfercanas <fernando.canas@outlook.com>
2022-05-19 17:57:09 -0500

    add/ reduce footer logo at 550px screen

--- src/components/Logos/styles.js
+++ src/components/Logos/styles.js
@@ -14,6 +14,10 @@ const StyledFooterLogo = styled.i`
   color: var(--n100);
   font-style: normal;
   font-size: 42px;
+
+  @media screen and (max-width: 550px) {
+    font-size: 32px;
+  }
 `;
 
 export { StyledHeaderLogo, StyledFooterLogo };
