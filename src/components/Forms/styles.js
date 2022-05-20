909ee78a6de4888e4ba00813a03a61a09363de31
wfercanas <fernando.canas@outlook.com>
2022-05-20 15:28:57 -0500

    add/ responsive contact form

--- src/components/Forms/styles.js
+++ src/components/Forms/styles.js
@@ -5,17 +5,31 @@ const StyledForm = styled.form`
   grid-template-columns: auto auto;
   column-gap: 44px;
   row-gap: 8px;
+
+  @media screen and (max-width: 550px) {
+    grid-template-columns: auto;
+    justify-content: center;
+  }
 `;
 
 const StyledTextArea = styled.div`
   grid-column: 1 / span 2;
+
+  @media screen and (max-width: 550px) {
+    grid-column: 1 / 2;
+  }
 `;
 
 const StyledButton = styled.div`
   margin-top: 16px;
   display: flex;
   justify-content: flex-end;
-  grid-column: 1 / span 2;
+  grid-column: 2 / span 1;
+
+  @media screen and (max-width: 550px) {
+    grid-column: 1 / 2;
+    justify-content: center;
+  }
 `;
 
 export { StyledForm, StyledTextArea, StyledButton };
