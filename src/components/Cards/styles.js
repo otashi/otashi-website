95313b0d07b0087bb73d93199a9ad0f5acbfb7fb
wfercanas <fernando.canas@outlook.com>
2022-05-20 14:51:16 -0500

    add/ services card responsive

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -5,7 +5,7 @@ const StyledServiceCard = styled.div`
   height: 200px;
   padding: 28px;
   display: grid;
-  row-gap: 32px;
+  row-gap: 16px;
   border: 1px solid var(--n50);
   border-radius: 16px;
   background-color: var(--n20);
@@ -13,27 +13,64 @@ const StyledServiceCard = styled.div`
   &:hover {
     border-color: var(--t500);
     background-color: var(--t50);
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 1000px) {
+    height: 150px;
+    padding: 16px;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 350px;
+    height: auto;
   }
 `;
 
-const StyledServiceCardContent = styled.div``;
+const StyledServiceCardContent = styled.div`
+  @media screen and (max-width: 1000px) {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: auto auto;
+    column-gap: 16px;
+    row-gap: 8px;
+  }
+`;
 
 const StyledServiceIcon = styled.img`
   width: 32px;
+
+  @media screen and (max-width: 1000px) {
+    width: 24px;
+  }
 `;
 
 const StyledServiceName = styled.p`
   font-size: 24px;
   font-weight: bold;
   margin: 8px 0;
+
+  @media screen and (max-width: 1000px) {
+    font-size: 16px;
+    margin: 0;
+  }
 `;
 
-const StyledServiceDescription = styled.p``;
+const StyledServiceDescription = styled.p`
+  margin: 0;
+  grid-column: 1 / span 2;
+
+  @media screen and (max-width: 800px) {
+    grid-column: 2 / span 1;
+  }
+`;
 
 const StyledServiceCardAction = styled.a`
   font-weight: bold;
   display: inline;
+  align-self: flex-end;
   width: 64px;
+  height: fit-content;
   color: var(--n900);
   padding-bottom: 4px;
   text-decoration: none;
@@ -42,6 +79,10 @@ const StyledServiceCardAction = styled.a`
 
   &:hover {
     width: 100px;
+  }
+
+  @media screen and (max-width: 800px) {
+    display: none;
   }
 `;
 
--- src/sections/Services/styles.js
+++ src/sections/Services/styles.js
@@ -4,6 +4,7 @@ const StyledServiceCardsContainer = styled.div`
   display: flex;
   gap: 16px;
   justify-content: center;
+  flex-wrap: wrap;
 `;
 
 export { StyledServiceCardsContainer };
