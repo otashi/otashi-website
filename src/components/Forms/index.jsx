5e767548de88f87fea6a557b825ab5f7dd2d12f3
wfercanas <fernando.canas@outlook.com>
2022-05-20 23:16:57 -0500

    add/ input change handler as props and state value

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -1,19 +1,55 @@
-import React from "react";
-import { Button } from "../Buttons";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/context";
 
+import { Button } from "../Buttons";
 import { Input, TextArea } from "../Inputs";
 
 import { StyledForm, StyledTextArea, StyledButton } from "./styles";
 
 const ContactForm = () => {
+  const { formData, setFormData } = useContext(AppContext);
+
+  const handleChange = ({ target }, payload) => {
+    setFormData({ ...formData, [payload]: target.value });
+  };
+
   return (
     <StyledForm>
-      <Input type="text" placeholder="Nombre" />
-      <Input type="text" placeholder="Teléfono" />
-      <Input type="email" placeholder="Correo electrónico" />
-      <Input type="text" placeholder="Servicio" />
+      <Input
+        type="text"
+        handleChange={handleChange}
+        value={formData.name}
+        id="name"
+        placeholder="Nombre"
+      />
+      <Input
+        type="text"
+        value={formData.phone}
+        handleChange={handleChange}
+        id="phone"
+        placeholder="Teléfono"
+      />
+      <Input
+        type="email"
+        value={formData.email}
+        handleChange={handleChange}
+        id="email"
+        placeholder="Correo electrónico"
+      />
+      <Input
+        type="text"
+        value={formData.service}
+        handleChange={handleChange}
+        id="service"
+        placeholder="Servicio"
+      />
       <StyledTextArea>
-        <TextArea placeholder="Cuéntanos sobre tu proyecto" />
+        <TextArea
+          value={formData.comment}
+          handleChange={handleChange}
+          id="comment"
+          placeholder="Cuéntanos sobre tu proyecto"
+        />
       </StyledTextArea>
       <StyledButton>
         <Button label="Enviar" type="submit" />
